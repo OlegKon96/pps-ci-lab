056d7e1a57e39a50985c4dad504c694d629a496c
Oleg Konchenkov <oleg.konchenkov@studio.unibo.it>
2020-05-04 15:12:36 +0200

    add javadoc

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,4 +1,8 @@
-public class HelloWorld {
+public class HelloWorld{
+    /**
+     * Simple Hello World
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
